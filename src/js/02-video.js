7e0bbcbb726ff71aa68b0d3c5ebce6a7efbefb62
Tanya Kuchma <kuchma.tanya@ukr.net>
2021-12-15 20:40:26 +0200

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+const keyTime = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+playerReload();
+
+function onTimeUpdate (evt) {
+    const seconds = evt.seconds;
+    console.log(seconds);
+    localStorage.setItem(keyTime, seconds)
+};
+
+function playerReload() {
+    const savedTime = localStorage.getItem(keyTime);
+    if (savedTime) {
+        player.setCurrentTime(savedTime);
+    }
+};
