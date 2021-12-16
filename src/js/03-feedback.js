9bb7595cfacf8840e2b94b0b8af09f5a0e295808
Tanya Kuchma <kuchma.tanya@ukr.net>
2021-12-16 17:27:41 +0200

    add task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
 
+import throttle from 'lodash.throttle';
+
+const storageKey = "feedback-form-state";
+let formData = {};
+const savedMessage = localStorage.getItem(storageKey);
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form  input'),
+    textarea: document.querySelector('.feedback-form  textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+fillFormLabels();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(formData);
+    evt.currentTarget.reset();
+    localStorage.removeItem(storageKey);
+    formData = {};
+};
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+
+function fillFormLabels() {
+    if (savedMessage) {   
+        refs.input.value = JSON.parse(savedMessage).email;
+        refs.textarea.value = JSON.parse(savedMessage).message;
+    }
+}
